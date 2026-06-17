af85675733612a6c250318f5f7f192a4a95397d6
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2026-06-18 05:30:04 +0700

    Update JellyCatDaily.py

--- JellyCatDaily.py
+++ JellyCatDaily.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import base64
 
+def load_css():
+    with open("styles.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
 st.set_page_config(layout="wide")
 
 with open("JellyCatDaily.png", "rb") as f:
